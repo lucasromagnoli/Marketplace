822d3f4e31f46d1cc4854bc78db145b38d7a5af3
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-05-13 22:34:00 -0300

    Feature - Middleware para verificar se o usuário está autenticado.

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,3 +1,21 @@
-module.exports = (req, res, next) => {
-  next()
+const authConfig = require('../../config/auth')
+const jwt = require('jsonwebtoken')
+const { promisify } = require('util')
+
+module.exports = async (req, res, next) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token não encontrado' })
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret)
+    req.userId = decoded.id
+    return next()
+  } catch (error) {
+    return res.status(401).json('error: Token invalido')
+  }
 }
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,13 @@ const express = require('express')
 
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const authMiddleware = require('./app/middlewares/auth')
 
 const routes = express.Router()
 
+routes.get('/teste', authMiddleware, (req, res) => {
+  return res.json({ id: req.userId })
+})
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
 module.exports = routes
